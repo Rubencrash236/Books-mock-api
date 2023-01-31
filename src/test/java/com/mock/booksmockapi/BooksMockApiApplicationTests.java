e10b42f5d55fe0121a21a4943c2f11b9e5ae2e4c
Ruben Díaz <rubenosmani.dc@outlook.com>
2023-01-31 17:36:31 -0400

    First commit

---
+++ src/test/java/com/mock/booksmockapi/BooksMockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mock.booksmockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksMockApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
